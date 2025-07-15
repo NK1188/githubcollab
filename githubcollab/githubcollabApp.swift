2f0b3ea32126babd34fac60cac4797496dff2780
Navya <navyazhni@gmail.com>
2025-07-15 07:15:41 -0700

    Initial Commit

---
+++ githubcollab/githubcollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubcollabApp.swift
+//  githubcollab
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct githubcollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ githubcollabTests/githubcollabTests.swift
@@ -0,0 +1,17 @@
+//
+//  githubcollabTests.swift
+//  githubcollabTests
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import Testing
+@testable import githubcollab
+
+struct githubcollabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
