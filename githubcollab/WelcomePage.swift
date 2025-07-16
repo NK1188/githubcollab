34022f3529dbe0565eca68b657bdba5a1e70f285
Navya <navyazhni@gmail.com>
2025-07-16 10:27:10 -0700

    welcome pg outline

--- githubcollab/WelcomePage.swift
+++ githubcollab/WelcomePage.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct WelcomePage: View {
     var body: some View {
-        Text("Welcome to the home page!")
+        VStack{
+            Text("Welcome Back!")
+                .padding()
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+            //or wtv color edit that
+                .background(Color(.systemGray6))
+                .cornerRadius(10)
+                .shadow(color: .gray.opacity(0.4), radius: 5, x: 0, y: 4)
+        }
+        .padding(.bottom, 50)
+        .padding(.top, 50)
+        VStack{
+            Text("Contents")
+            //idk what to call it so change the name later
+            VStack{
+                Text("Mood Tracker + Daily log in")
+            }
+            .padding(20)
+            VStack{
+                Text("Personal Journal")
+            }
+            .padding(20)
+            VStack{
+                Text("General Journal")
+            }
+            .padding(20)
+        }
+        .padding(.bottom,200)
+        .padding(.top, 100)
     }
 }
 
