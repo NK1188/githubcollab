4e2e9e333ef0abfa2f8f5e23bcc3a048b925cb2f
Navya <navyazhni@gmail.com>
2025-07-16 10:03:02 -0700

    sign in page outline

---
+++ githubcollab/WelcomePage.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomePage.swift
+//  githubcollab
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text("Welcome to the home page!")
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
